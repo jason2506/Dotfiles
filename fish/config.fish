124dc9b80adbd7a8742d563217baa9ced0edc3cb
Chi-En Wu <jason2506.mail@gmail.com>
2016-06-03 09:56:47 +0800

    Refine `config.fish`

--- fish/config.fish
+++ fish/config.fish
@@ -32,29 +32,19 @@ end
 set fish_greeting ""
 set BROWSER 'open'
 
-set -x LANGUAGE zh_TW:zh:en_US:en
-set -x LANG zh_TW.UTF-8
-set -x LC_COLLATE zh_TW.UTF-8
-set -x LC_CTYPE zh_TW.UTF-8
-set -x LC_MESSAGES zh_TW.UTF-8
-set -x LC_MONETARY zh_TW.UTF-8
-set -x LC_NUMERIC zh_TW.UTF-8
-set -x LC_TIME zh_TW.UTF-8
-set -x LC_ALL zh_TW.UTF-8
-
-set -x PYENV_ROOT ~/.pyenv
-set -x PATH ~/.cabal/bin $PYENV_ROOT/bin /usr/local/bin /usr/local/share/npm/bin $PATH
+set -x LANGUAGE en_US:zh:en_US:en
+set -x LANG en_US.UTF-8
+set -x LC_COLLATE en_US.UTF-8
+set -x LC_CTYPE en_US.UTF-8
+set -x LC_MESSAGES en_US.UTF-8
+set -x LC_MONETARY en_US.UTF-8
+set -x LC_NUMERIC en_US.UTF-8
+set -x LC_TIME en_US.UTF-8
+set -x LC_ALL en_US.UTF-8
 
 set -x TERM xterm-256color
 set -x CLICOLOR 1
 set -x LS_COLORS "di=00;36"
 set fish_color_command yellow
 
-if begin; status --is-interactive; and type -f pyenv > /dev/null; end
-    . (pyenv init -|psub)
-end
-
-if begin; status --is-interactive; and type -f pyenv-virtualenv-init > /dev/null; end
-    . (pyenv virtualenv-init -|psub)
-end
-
+alias nvm "bass source ~/.nvm/nvm.sh --no-use ';' nvm"
