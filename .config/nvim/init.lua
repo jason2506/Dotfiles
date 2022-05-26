0e654fc63239a6653f536fd627c7dd05c6ebe938
Chi-En Wu <jason2506@users.noreply.github.com>
2022-05-26 09:44:02 +0800

    Add keymap for changing working directory

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -84,4 +84,19 @@ keymap.set('n', 'bT', ':bprev<CR>', keymap_opts)
 keymap.set('v', '>', '>gv', keymap_opts)
 keymap.set('v', '<', '<gv', keymap_opts)
 
+keymap.set('n', '<leader>cd', function ()
+  local default_path = vim.fn.expand('%:~:.:h')
+  local input_opts = {
+    prompt = 'Set working directory: ',
+    default = default_path,
+    completion = 'dir',
+  }
+
+  vim.ui.input(input_opts, function (target_path)
+    if target_path then
+      vim.api.nvim_set_current_dir(target_path)
+    end
+  end)
+end, keymap_opts)
+
 require('plugins')
