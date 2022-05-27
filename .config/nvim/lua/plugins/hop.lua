e695da1a97a7e965b3f8a85859217e129345ca84
Chi-En Wu <jason2506@users.noreply.github.com>
2022-05-27 10:12:18 +0800

    Extract `hot_hint`

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -1,15 +1,16 @@
 local hop = require('hop')
+local hop_hint = require('hop.hint')
 local keymap = require('utils.keymap')
 
 hop.setup()
 
 keymap.set('', 't', function ()
   hop.hint_char1({
-    direction = require("hop.hint").HintDirection.AFTER_CURSOR,
+    direction = hop_hint.HintDirection.AFTER_CURSOR,
   })
 end)
 keymap.set('', 'T', function ()
   hop.hint_char1({
-    direction = require("hop.hint").HintDirection.BEFORE_CURSOR,
+    direction = hop_hint.HintDirection.BEFORE_CURSOR,
   })
 end)
