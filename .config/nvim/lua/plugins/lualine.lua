bb39a0543b10b7b27af904ba3b62b2fce3170041
Chi-En Wu <jason2506@users.noreply.github.com>
2022-05-27 11:26:50 +0800

    Refine lualine config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,25 @@
 require('lualine').setup({
   options = {
     icons_enabled = false,
-    component_separators = '·',
-  }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+      { 'filename', path = 1 },
+    },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+      { 'filename', path = 1 },
+    },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  },
 })
