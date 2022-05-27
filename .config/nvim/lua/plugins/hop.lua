4a66f8c9f452ff25f9c27fd617d85e478ad2e741
Chi-En Wu <jason2506@users.noreply.github.com>
2022-05-27 21:58:47 +0800

    Refine keymaps for `hop`

--- .config/nvim/lua/plugins/hop.lua
+++ .config/nvim/lua/plugins/hop.lua
@@ -4,13 +4,35 @@ local keymap = require('utils.keymap')
 
 hop.setup()
 
-keymap.set('', 't', function ()
-  hop.hint_char1({
-    direction = hop_hint.HintDirection.AFTER_CURSOR,
+local function set_hop_hint_keymap(map_key, opts)
+  local inclusive_opts = vim.tbl_extend('force', opts, {
+    inclusive_jump = true,
   })
-end)
-keymap.set('', 'T', function ()
-  hop.hint_char1({
-    direction = hop_hint.HintDirection.BEFORE_CURSOR,
-  })
-end)
+
+  keymap.set({ 'n', 'v' }, map_key, function ()
+    hop.hint_char1(opts)
+  end)
+
+  keymap.set('o', map_key, function ()
+    vim.opt_local.virtualedit = 'onemore'
+    hop.hint_char1(inclusive_opts)
+    vim.opt_local.virtualedit = ''
+  end)
+end
+
+local function set_hop_hint_keymap_bi(map_hint_after, map_hint_before, opts)
+  local hint_after_opts = { direction = hop_hint.HintDirection.AFTER_CURSOR }
+  local hint_before_opts = { direction = hop_hint.HintDirection.BEFORE_CURSOR }
+  if opts then
+    hint_after_opts = vim.tbl_extend('force', hint_after_opts, opts)
+    hint_before_opts = vim.tbl_extend('force', hint_before_opts, opts)
+  end
+
+  set_hop_hint_keymap(map_hint_after, hint_after_opts)
+  set_hop_hint_keymap(map_hint_before, hint_before_opts)
+end
+
+set_hop_hint_keymap_bi('t', 'T')
+set_hop_hint_keymap_bi('f', 'F', {
+  current_line_only = true,
+})
