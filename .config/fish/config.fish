b7a51edb241080c69d0191a86f7e9dcd66ba5571
Chi-En Wu <jason2506@users.noreply.github.com>
2022-06-01 22:31:09 +0800

    Use `$HOME` instead of `~`

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set fish_greeting
 
 # pnpm
-set -gx PNPM_HOME "~/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
